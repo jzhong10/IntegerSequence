b64fd7f2de40543abc16db2492714b9418ef27c1
jzhong10 <junzhong2003@gmail.com>
2020-12-04 17:35:36 -0500

    fixed Range.java to include last number in range, use the provided Tester to print out a sequence of numbers

--- Tester.java
+++ Tester.java
@@ -2,5 +2,17 @@ public class Tester {
     public static void main(String[] args) {
         Range x = new Range(1, 5);
         System.out.println(x.length());
+        Range y = new Range(14, 29);
+        System.out.println(y.length());
+
+        IntegerSequence r = new Range(10,15);
+        while(r.hasNext()){
+          System.out.print(r.next());
+          if( r.hasNext() ){
+             System.out.print( ", " );
+          }
+        }
+        System.out.println();
+        //System.out.println(r.hasNext());
     }
 }
