e343186515cff69c86c0b89565f7d6550a42a69b
jzhong10 <junzhong2003@gmail.com>
2020-12-08 12:50:42 -0500

    Tested ArraySequence with int[] constructor

--- Tester.java
+++ Tester.java
@@ -17,5 +17,12 @@ public class Tester {
         r.reset();
         System.out.println(r.next());
 
+        int[] nums = {1,3,5,0,-1,3,9};
+        IntegerSequence as = new ArraySequence(nums);
+
+        System.out.println("ArraySequence(array):");
+        while(as.hasNext()){
+          System.out.print(as.next()+", ");
+        }
     }
 }
